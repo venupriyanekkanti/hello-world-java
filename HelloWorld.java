7c17b561648049f2af02f58d1b431b549eccee8d
root <root@ip-172-31-95-97.ec2.internal>
2022-08-29 10:58:03 +0000

    committed

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,9 +36,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, priya");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
+    System.out.println("Hello,have a greatday");
   }
 }
